b8ee58a2a17e3c9bf73fdea62e31001f1e049d18
DaniPPereira <a23507@alunos.ipca.pt>
2025-01-31 15:53:56 +0000

    Deploy

--- User_Service/src/usecases/User/registerUser.js
+++ User_Service/src/usecases/User/registerUser.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const userRepository = require('../../framework/db/postgres/UserRepository'); // Importa o repositório do usuário
 
