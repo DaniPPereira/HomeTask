d0fd409deb59b328c12c8386daab88950b4be5b4
DaniPPereira <a23507@alunos.ipca.pt>
2025-02-01 00:30:17 +0000

    Deploy

--- User_Service/src/usecases/User/registerUser.js
+++ User_Service/src/usecases/User/registerUser.js
@@ -5,11 +5,11 @@ const userRepository = require('../../framework/db/postgres/UserRepository'); //
 const SECRET_KEY = process.env.JWT_SECRET; // Chave secreta para o JWT
 
 module.exports = async (userData) => {
-    const { name, email, password } = userData;
+    const { name, email, password, role } = userData;
 
     // Validação dos campos obrigatórios
-    if (!name || !email || !password) {
-        throw new Error('Missing required fields: name, email, password');
+    if (!name || !email || !password || !role) {
+        throw new Error('Missing required fields: name, email, password, role');
     }
 
     // Verifica se o email já está em uso
@@ -27,7 +27,7 @@ module.exports = async (userData) => {
         name,
         email,
         password: hashedPassword,
-        roles: ['user'] // Role padrão
+        role: role
     });
 
     // Gerar token JWT
@@ -35,7 +35,7 @@ module.exports = async (userData) => {
         { 
             id: newUser.id, 
             email: newUser.email, 
-            roles: newUser.roles 
+            role: newUser.role  // Também uma string simples aqui
         },
         SECRET_KEY,
         { expiresIn: '1h' }
@@ -46,7 +46,7 @@ module.exports = async (userData) => {
         id: newUser.id,
         name: newUser.name,
         email: newUser.email,
-        roles: newUser.roles,
+        role: newUser.role,
         token
     };
 };
