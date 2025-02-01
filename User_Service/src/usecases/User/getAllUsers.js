ed735ad9267e55dba94a2c3e684b74c2509a0a4a
DaniPPereira <a23507@alunos.ipca.pt>
2025-02-01 01:21:23 +0000

    Deploy

--- User_Service/src/usecases/User/getAllUsers.js
+++ User_Service/src/usecases/User/getAllUsers.js
@@ -2,9 +2,9 @@ const UserRepository = require('../../framework/db/postgres/UserRepository');
 
 module.exports = async () => {
    
-    const shoppingList = await UserRepository.findAll();
+    const users = await UserRepository.findAll();
     return {
         status: 200,
-        shoppingList,
+        users,
     };
 };
